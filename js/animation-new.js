aaab4ff0ef5f153048a359e9af1a0dde8c227799
Michał Sapalski <sapalskimichal@gmail.com>
2013-11-22 19:46:46 +0100

    Rewrite: animation works

--- js/animation-new.js
+++ js/animation-new.js
@@ -55,17 +55,19 @@ Keyframe.prototype.crop = function() {
   this.offset = Math.max(0.0, Math.min(1.0, this.offset));
 };
 
-var Actor = function(id, image, width) {
+var Actor = function(id, image, width, duration) {
   this.id = id;
   this.image = image;
   this.width = width;
+  this.duration = duration;
+  this.recordedKeyframes = [];
+  this.recordedType = null;
   this.rootElement = null;
   this.keyframes = {};
   this.elements = {};
   this.animations = {};
   this.players = {};
-  for (var i = 0; i < Keyframe.types.length; ++i) {
-    var type = Keyframe.types[i];
+  for (var type in Keyframe.types) {
     this.keyframes[type] = [];
   }
 };
@@ -94,47 +96,64 @@ Actor.prototype.createElements = function(camera) {
 Actor.prototype.deleteElements = function() {
   this.rootElement.remove();
 };
-Actor.prototype.generateAnimation = function(duration) {
-  for (var i = 0; i < Keyframe.types.length; ++i) {
-    var type = Keyframe.types[i];
-    var transforms = [];
-    var croppedKeyframes = keyframes.slice(0);
-    croppedKeyframes.sort(function(k1, k2) {return k1.offset - k2.offset;});
-    for (var i = 0; i < croppedKeyframes.length; ++i) {
-      croppedKeyframes[i].crop();
-    }
-    if (croppedKeyframes.length === 0) {
-      croppedKeyframes.push(Keyframe.ofType(0.0, type));
-    }
-    var first = croppedKeyframes[0];
-    var last = croppedKeyframes[croppedKeyframes.length - 1];
-    if (first.offset !== 0.0) {
-      croppedKeyframes.unshift(first.copy(0.0));
-    }
-    if (last.offset !== 1.0) {
-      croppedKeyframes.push(last.copy(1.0));
-    }
-    for (var i = 0; i < croppedKeyframes.length; ++i) {
-      var keyframe = croppedKeyframes[i];
-      transforms.push({
-        offset: keyframe.offset,
-        transform: keyframe.value.tranfsormText(),
-      });
-    }
-    animations[type] = new Animation(elements[type], transforms, {duration: duration});
+Actor.prototype.generateAnimationType = function(type) {
+  var transforms = [];
+  var croppedKeyframes = this.keyframes[type].slice(0);
+  croppedKeyframes.sort(function(k1, k2) {return k1.offset - k2.offset;});
+  for (var i = 0; i < croppedKeyframes.length; ++i) {
+    croppedKeyframes[i].crop();
+  }
+  if (croppedKeyframes.length === 0) {
+    croppedKeyframes.push(Keyframe.ofType(0.0, type));
+  }
+  var first = croppedKeyframes[0];
+  var last = croppedKeyframes[croppedKeyframes.length - 1];
+  if (first.offset !== 0.0) {
+    croppedKeyframes.unshift(first.copy(0.0));
+  }
+  if (last.offset !== 1.0) {
+    croppedKeyframes.push(last.copy(1.0));
+  }
+  for (var i = 0; i < croppedKeyframes.length; ++i) {
+    var keyframe = croppedKeyframes[i];
+    transforms.push({
+      offset: keyframe.offset,
+      transform: keyframe.value.tranfsormText(),
+    });
+  }
+  this.animations[type] = new Animation(this.elements[type], transforms, {duration: this.duration});
+};
+Actor.prototype.generateAnimation = function() {
+  for (var type in Keyframe.types) {
+    this.generateAnimationType(type);
   }
 };
+Actor.prototype.saveRecordedKeyframes = function() {
+  if (this.recordedKeyframes.length === 0) {
+    return;
+  }
+  var minTime = this.recordedKeyframes[0].offset;
+  var maxTime = minTime;
+  for (var i = 0; i < this.recordedKeyframes.length; ++i) {
+    var time = this.recordedKeyframes[i].offset;
+    minTime = Math.min(minTime, time);
+    maxTime = Math.max(maxTime, time);
+  }
+  var result = this.keyframes[this.recordedType].filter(function(k) {
+    return k.offset < minTime || maxTime < k.offset;
+  });
+  this.keyframes[this.recordedType] = result.concat(this.recordedKeyframes);
+  this.generateAnimationType(this.recordedType);
+};
 Actor.prototype.pause = function() {
-  for (var i = 0; i < Keyframe.types.length; ++i) {
-    var type = Keyframe.types[i];
+  for (var type in Keyframe.types) {
     if (this.players[type]) {
       this.players[type].paused = true;
     }
   }
 };
 Actor.prototype.unpause = function() {
-  for (var i = 0; i < Keyframe.types.length; ++i) {
-    var type = Keyframe.types[i];
+  for (var type in Keyframe.types) {
     if (this.players[type]) {
       this.players[type].paused = false;
     }
@@ -150,32 +169,49 @@ Actor.prototype.stopType = function(type) {
   }
 }
 Actor.prototype.stop = function() {
-  for (var i = 0; i < Keyframe.types.length; ++i) {
-    var type = Keyframe.types[i];
+  for (var type in Keyframe.types) {
     this.stopType(type);
   }
 };
 Actor.prototype.playType = function(type) {
-  this.players[type] = document.timeline.play(this.animations[type]);
-}
+  if (this.animations[type]) {
+    this.players[type] = document.timeline.play(this.animations[type]);
+  }
+};
 Actor.prototype.play = function() {
-  for (var i = 0; i < Keyframe.types.length; ++i) {
-    var type = Keyframe.types[i];
+  for (var type in Keyframe.types) {
     this.playType(type);
   }
 };
 Actor.prototype.startRecording = function(type) {
-  this.recordedKeyframes = [];
   this.recordedType = type;
   this.stopType(type);
 };
 Actor.prototype.recordKeyframe = function(keyframe) {
   this.recordedKeyframes.push(keyframe);
+  var element = this.elements[this.recordedType];
+  var transform = keyframe.value.tranfsormText();
+  if (element.style._clearAnimatedProperty) {
+    element.style._clearAnimatedProperty("webkitTransform");
+  }
+  element.style.transform = transform;
+  element.style.webkitTransform = transform;
+  element.style.mozTransform = transform;
 };
-Actor.prototype.endRecording = function(type, offset) {
-  this.updateAnimation(type, recordedKeyframes);
-  this.playType(type);
-  this.seekType(type, offset);
+Actor.prototype.endRecording = function(offset, play) {
+  if (!this.recordedType) {
+    return;
+  }
+  this.saveRecordedKeyframes();
+  if (play) {
+    this.playType(this.recordedType);
+    this.seekType(this.recordedType, offset);
+  }
+  this.recordedType = null;
+  this.recordedKeyframes = [];
+};
+Actor.prototype.seekType = function(type, offset) {
+  this.players[type].currentTime += offset * this.duration;
 };
 Actor.prototype.select = function(selected) {
   if (selected) {
@@ -185,19 +221,20 @@ Actor.prototype.select = function(selected) {
   }
 };
 
-var Player = function(cameraElement, progressElement) {
+var Player = function(cameraElement, progressElement, duration) {
   this.actors = [];
   this.selected = 0;
   this.cameraElement = cameraElement;
   this.progressElement = progressElement;
-  this.player = null;
+  this.duration = duration;
+  this.progressPlayer = null;
 };
 Player.prototype = {};
 Player.prototype.playing = function() {
-  return this.player && this.player.paused == false;
+  return this.progressPlayer && this.progressPlayer.paused == false;
 };
 Player.prototype.addActor = function(id, image, width) {
-  var a = new Actor(id, image, width);
+  var a = new Actor(id, image, width, this.duration);
   a.createElements(this.cameraElement);
   this.actors.push(a);
   if (this.selected === this.actors.length) {
@@ -229,16 +266,67 @@ Player.prototype._updateSelected = function() {
   }
 };
 Player.prototype.selectNextActor = function() {
+  this.endRecording();
   this.selected = (this.selected + 1) % (this.actors.length + 1);
   this._updateSelected();
+};
+Player.prototype._withSelected = function(f) {
+  if (this.selected === this.actors.length) {
+    return;
+  }
+  return f(this.actors[this.selected]);
+};
+Player.prototype.startRecording = function(type) {
+  this._withSelected(function(a) {
+    a.startRecording(type);
+  });
+};
+Player.prototype.recordKeyframe = function(keyframe) {
+  this._withSelected(function(a) {
+    a.recordKeyframe(keyframe);
+  });
+};
+Player.prototype.endRecording = function() {
+  var position = this.position();
+  var playing = this.playing();
+  this._withSelected(function(a) {
+    a.endRecording(position, playing);
+  });
+};
+Player.prototype.play = function() {
+  if (this.playing()) {
+    return;
+  }
+  this.progressPlayer = document.timeline.play(new Animation(
+        this.progressElement, [
+          {offset: 0.0, width: "0%"},
+          {offset: 1.0, width: "100%"},
+        ], this.duration));
+  for (var i = 0; i < this.actors.length; ++i) {
+    this.actors[i].play();
+  }
+};
+Player.prototype.stop = function() {
+  this.progressPlayer._deregisterFromTimeline(); 
+  this.progressPlayer.paused = true;
+  this.progressPlayer.source = null;
+  for (var i = 0; i < this.actors.length; ++i) {
+    this.actors[i].stop();
+  }
+}
+Player.prototype.position = function() {
+  if (!this.progressPlayer) {
+    return 0.0;
+  }
+  return Math.min(1.0, this.progressPlayer.currentTime / this.duration);
 };
 
 var player = null;
 
 window.addEventListener("load", function() {
   var camera = document.getElementById("camera");
-  var progress = document.getElementById("progress");
-  player = new Player(camera, progress);
+  var progress = document.getElementById("bar");
+  player = new Player(camera, progress, 15);
 
   var someActors = [{
     "id": "cat-left",
@@ -274,5 +362,11 @@ window.addEventListener("load", function() {
       e.preventDefault();
     }
   });
+
+  var cat = player.getActor("cat-right");
+  cat.startRecording("translation");
+  cat.recordKeyframe(new Keyframe(0.1, new Position(0, 0.9)));
+  cat.recordKeyframe(new Keyframe(0.2, new Position(3.5, 2)));
+  cat.endRecording(0.0);
 });
 
